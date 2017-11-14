06f0d466ca14e65f6eb43e0a59512486c9ffd098
Robert Linde <robert@selberglinde.se>
2017-11-14 15:22:34 +0100

    Add basic error handling

--- TheExampleApp/Pages/Error.cshtml.cs
+++ TheExampleApp/Pages/Error.cshtml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Contentful.Core.Errors;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TheExampleApp.Pages
@@ -13,9 +15,18 @@ namespace TheExampleApp.Pages
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        public ContentfulException ContentfulException { get; set; }
+
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            var ex = HttpContext?.Features?.Get<IExceptionHandlerFeature>()?.Error;
+
+            if(ex is ContentfulException)
+            {
+                ContentfulException = ex as ContentfulException;
+                RequestId = ContentfulException.RequestId;
+            }
         }
     }
 }
--- TheExampleApp/Startup.cs
+++ TheExampleApp/Startup.cs
@@ -62,6 +62,7 @@ namespace TheExampleApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStatusCodePages();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
