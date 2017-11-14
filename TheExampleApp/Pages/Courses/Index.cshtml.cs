1f1f1d6f21cdc4aab37f47fc60e934c3d4666781
Robert Linde <robert@selberglinde.se>
2017-11-14 17:52:53 +0100

    Add some better 404 handling

--- TheExampleApp/Pages/Courses/Index.cshtml.cs
+++ TheExampleApp/Pages/Courses/Index.cshtml.cs
@@ -23,13 +23,20 @@ namespace TheExampleApp.Pages.Courses
             _breadcrumbsManager = breadcrumbsManager;
         }
 
-        public async Task OnGet(string slug)
+        public async Task<IActionResult> OnGet(string slug)
         {
             var queryBuilder = QueryBuilder<Course>.New.ContentTypeIs("course").FieldEquals(f => f.Slug, slug?.ToLower()).Include(5).LocaleIs(CultureInfo.CurrentCulture.ToString());
             Course = (await _client.GetEntries(queryBuilder)).FirstOrDefault();
+
+            if(Course == null)
+            {
+                return NotFound();
+            }
+
             _visitedLessonsManager.AddVisitedLesson(Course.Sys.Id);
             _breadcrumbsManager.ReplaceCrumbForSlug(Course.Slug.ToLower().Replace('-', ' '), Course.Title);
             ViewData["VisitedLessons"] = _visitedLessonsManager.VisitedLessons;
+            return Page();
         }
 
         public Course Course { get; set; }
--- TheExampleApp/Pages/Courses/Lessons.cshtml.cs
+++ TheExampleApp/Pages/Courses/Lessons.cshtml.cs
@@ -23,17 +23,30 @@ namespace TheExampleApp.Pages.Courses
             _breadcrumbsManager = breadcrumbsManager;
         }
 
-        public async Task OnGet(string slug, string lessonSlug)
+        public async Task<IActionResult> OnGet(string slug, string lessonSlug)
         {
             var queryBuilder = QueryBuilder<Course>.New.ContentTypeIs("course").FieldEquals(f => f.Slug, slug?.ToLower()).Include(5).LocaleIs(CultureInfo.CurrentCulture.ToString());
             Course = (await _client.GetEntries(queryBuilder)).FirstOrDefault();
-            _breadcrumbsManager.ReplaceCrumbForSlug(Course.Slug.ToLower().Replace('-', ' '), Course.Title);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
 
             SelectedLesson = Course.Lessons.FirstOrDefault(c => c.Slug == lessonSlug?.ToLower());
-            _breadcrumbsManager.ReplaceCrumbForSlug(SelectedLesson.Slug.ToLower().Replace('-', ' '), SelectedLesson.Title);
 
+            if (SelectedLesson == null)
+            {
+                return NotFound();
+            }
+
+            _breadcrumbsManager.ReplaceCrumbForSlug(Course.Slug.ToLower().Replace('-', ' '), Course.Title);
+            _breadcrumbsManager.ReplaceCrumbForSlug(SelectedLesson.Slug.ToLower().Replace('-', ' '), SelectedLesson.Title);
             _visitedLessonsManager.AddVisitedLesson(SelectedLesson.Sys.Id);
+
             ViewData["VisitedLessons"] = _visitedLessonsManager.VisitedLessons;
+
+            return Page();
         }
 
         public Course Course { get; set; }
