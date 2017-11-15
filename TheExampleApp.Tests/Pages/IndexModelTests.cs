584173607c336746b9a7afa7e7a9b3ac8dfeec4c
Robert Linde <robert@selberglinde.se>
2017-11-15 21:05:00 +0100

    Add first page tests

---
+++ TheExampleApp.Tests/Pages/IndexModelTests.cs
@@ -0,0 +1,44 @@
+﻿using Contentful.Core;
+using Contentful.Core.Models;
+using Contentful.Core.Search;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TheExampleApp.Models;
+using TheExampleApp.Pages;
+using Xunit;
+
+namespace TheExampleApp.Tests.Pages
+{
+    public class IndexModelTests
+    {
+        [Fact]
+        public async Task GettingIndexShouldSetLayoutCorrectly()
+        {
+            //Arrange
+            var collection = new ContentfulCollection<Layout>();
+            collection.Items = new List<Layout>()
+            {
+                new Layout()
+                {
+                    Title = "SomeTitle"
+                }
+            };
+            var client = new Mock<IContentfulClient>();
+            client.SetupGet(c => c.SerializerSettings).Returns(new JsonSerializerSettings());
+            client.Setup(c => c.GetEntries(It.IsAny<QueryBuilder<Layout>>(), default(CancellationToken)))
+                .Returns(Task.FromResult(collection));
+            var model = new IndexModel(client.Object);
+            //Act
+
+            await model.OnGet();
+
+            //Assert
+            Assert.Equal("SomeTitle", model.IndexPage.Title);
+        }
+    }
+}
