9258f5b0338c8e6c19bbbdc223072180422963ca
Robert Linde <robert@selberglinde.se>
2017-11-16 14:49:30 +0100

    Add prettier 404 handling

--- TheExampleApp/Startup.cs
+++ TheExampleApp/Startup.cs
@@ -62,7 +62,7 @@ namespace TheExampleApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseStatusCodePages();
+            app.UseStatusCodePagesWithReExecute("/Error");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
