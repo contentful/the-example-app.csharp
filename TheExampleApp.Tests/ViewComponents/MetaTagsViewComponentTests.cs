ae480126f2310941e4082a3b1c278b85fa45e9c1
Robert Linde <robert@selberglinde.se>
2017-11-20 09:46:56 +0100

    Add metadata tests

---
+++ TheExampleApp.Tests/ViewComponents/MetaTagsViewComponentTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheExampleApp.ViewComponents;
+using Xunit;
+
+namespace TheExampleApp.Tests.ViewComponents
+{
+    public class MetaTagsViewComponentTests
+    {
+        [Theory]
+        [InlineData("Bongo")]
+        [InlineData("Banana")]
+        [InlineData("Hello")]
+        public void ComponentShouldSetProvidedTitleAsPartOfViewTitle(string title)
+        {
+            //Arrange
+            var localizer = new Mock<IViewLocalizer>();
+            localizer.SetupGet(c => c["defaultTitle"]).Returns(new LocalizedHtmlString("defaultTitle", "Knorr buljong"));
+            var component = new MetaTagsViewComponent(localizer.Object);
+            //Act
+            var res = component.Invoke(title);
+            //Assert
+            Assert.IsType<ViewViewComponentResult>(res);
+            Assert.Equal($"{title} — Knorr buljong", ((res as ViewViewComponentResult).ViewData.Model as string));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ComponentShouldSetDefaultTitleIfNoneIsProvided(string title)
+        {
+            //Arrange
+            var localizer = new Mock<IViewLocalizer>();
+            localizer.SetupGet(c => c["defaultTitle"]).Returns(new LocalizedHtmlString("defaultTitle", "Knorr buljong"));
+            var component = new MetaTagsViewComponent(localizer.Object);
+            //Act
+            var res = component.Invoke(title);
+            //Assert
+            Assert.IsType<ViewViewComponentResult>(res);
+            Assert.Equal("Knorr buljong", ((res as ViewViewComponentResult).ViewData.Model as string));
+        }
+    }
+}
