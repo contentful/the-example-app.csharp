889ce8d499a2da70504facb5d7b0571530f02819
Robert Linde <robert@selberglinde.se>
2017-11-20 09:09:56 +0100

    Add EntryState tests

---
+++ TheExampleApp.Tests/ViewComponents/EntryStateViewComponentTests.cs
@@ -0,0 +1,60 @@
+﻿using Contentful.Core.Configuration;
+using Contentful.Core.Models;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TheExampleApp.Configuration;
+using TheExampleApp.Tests.Pages;
+using TheExampleApp.ViewComponents;
+using Xunit;
+
+namespace TheExampleApp.Tests.ViewComponents
+{
+    public class EntryStateViewComponentTests
+    {
+        [Fact]
+        public async Task ComponentShouldSetDraftIfEntryIsNull()
+        {
+            //Arrange
+            var handler = new FakeMessageHandler();
+            handler.Responses.Enqueue(new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.NotFound, Content = new StringContent("{}") });
+            var httpClient = new HttpClient(handler);
+            var optionsManager = new Mock<IContentfulOptionsManager>();
+            optionsManager.SetupGet(c => c.Options).Returns(new ContentfulOptions());
+            var component = new EntryStateViewComponent(httpClient, optionsManager.Object);
+            var sysProperties = new SystemProperties();
+            sysProperties.Id = "434";
+            //Act
+            var res = await component.InvokeAsync(sysProperties);
+
+            //Assert
+            Assert.IsType<ViewViewComponentResult>(res);
+            Assert.True(((res as ViewViewComponentResult).ViewData.Model as EntryStateModel).Draft);
+        }
+
+        [Fact]
+        public async Task ComponentShouldSetPendingChangesIfUpdatedAtDoesNotMatch()
+        {
+            //Arrange
+            var handler = new FakeMessageHandler();
+            handler.Responses.Enqueue(new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.OK, Content = new StringContent(@"{""sys"": {""updatedAt"":""2017-10-01""}, ""fields"": {""test"": ""pop""}}") });
+            var httpClient = new HttpClient(handler);
+            var optionsManager = new Mock<IContentfulOptionsManager>();
+            optionsManager.SetupGet(c => c.Options).Returns(new ContentfulOptions());
+            var component = new EntryStateViewComponent(httpClient, optionsManager.Object);
+            var sysProperties = new SystemProperties();
+            sysProperties.UpdatedAt = new DateTime(2017, 11, 03);
+            sysProperties.Id = "123";
+            //Act
+            var res = await component.InvokeAsync(sysProperties);
+
+            //Assert
+            Assert.IsType<ViewViewComponentResult>(res);
+            Assert.True(((res as ViewViewComponentResult).ViewData.Model as EntryStateModel).PendingChanges);
+        }
+    }
+}
