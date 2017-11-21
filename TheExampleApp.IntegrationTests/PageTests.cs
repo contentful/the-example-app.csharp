4a5d9803c7a226b27a8de3445a35bb75ce0f7ea2
Robert Linde <robert@selberglinde.se>
2017-11-21 15:43:00 +0100

    Add integration tests for settings

--- TheExampleApp.IntegrationTests/PageTests.cs
+++ TheExampleApp.IntegrationTests/PageTests.cs
@@ -6,10 +6,14 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -186,6 +190,64 @@ namespace TheExampleApp.IntegrationTests
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
 
+        [Fact]
+        public async Task SettingsShouldReturn200()
+        {
+            // Act
+            var response = await _client.GetAsync("/Settings");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Contains("<h1>Settings</h1>",
+                responseString);
+        }
+
+        [Fact]
+        public async Task SettingsShouldReturn200ForGerman()
+        {
+            // Act
+            var response = await _client.GetAsync("/Settings?locale=de-DE");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Contains("<h1>Einstellungen</h1>",
+                responseString);
+        }
+
+        [Fact]
+        public async Task SettingsShouldTurnOnEditorialFeaturesAndReturn200()
+        {
+            // Act
+            var response = await _client.GetAsync("/Settings?enable_editorial_features=true");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Contains("<h1>Settings</h1>",
+                responseString);
+            Assert.Contains(@"<input type=""checkbox"" checked=""checked"" data-val=""true"" data-val-required=""The EnableEditorialFeatures field is required."" id=""AppOptions_EnableEditorialFeatures"" name=""AppOptions.EnableEditorialFeatures"" value=""true"" />",
+                responseString);
+        }
+
+        [Fact]
+        public async Task PostingToSwitchApiShouldSwitchAPIAndReturn302()
+        {
+            //Arrange
+            var formContent = await GetRequestContentAsync(_client, "/Settings", new Dictionary<string, string>
+                {
+                    { "api", "cpa" } ,
+                    { "prevPage", "/" },
+                });
+            // Act
+            var response = await _client.PostAsync("/Settings?handler=SwitchApi", formContent);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Found, response.StatusCode);
+        }
+
         protected virtual void InitializeServices(IServiceCollection services)
         {
             var startupAssembly = typeof(Startup).GetTypeInfo().Assembly;
@@ -219,6 +281,30 @@ namespace TheExampleApp.IntegrationTests
             while (directoryInfo.Parent != null);
 
             throw new Exception($"Project root could not be located using the application root {applicationBasePath}.");
+        }
+
+        private static async Task<FormUrlEncodedContent> GetRequestContentAsync(HttpClient _client, string path, IDictionary<string, string> data)
+        {
+            // Make a request for the resource.
+            var getResponse = await _client.GetAsync(path);
+
+            // Set the response's antiforgery cookie on the HttpClient.
+            _client.DefaultRequestHeaders.Add("Cookie", getResponse.Headers.GetValues("Set-Cookie"));
+
+            // Obtain the request verification token from the response.
+            // Any <form> element in the response contains a token, and
+            // they're all the same within a single response.
+            var responseMarkup = await getResponse.Content.ReadAsStringAsync();
+            var regExp_RequestVerificationToken = new Regex("<input name=\"__RequestVerificationToken\" type=\"hidden\" value=\"(.*?)\" \\/>", RegexOptions.Compiled);
+            var matches = regExp_RequestVerificationToken.Matches(responseMarkup);
+            // Group[1] represents the captured characters, represented
+            // by (.*?) in the Regex pattern string.
+            var token = matches?.FirstOrDefault().Groups[1].Value;
+
+            // Add the token to the form data for the request.
+            data.Add("__RequestVerificationToken", token);
+
+            return new FormUrlEncodedContent(data);
         }
     }
 }
